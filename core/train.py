bad3c15f3b1bb5675953be96adca0ede3c489815
Jiaxin Wang <jiaxin13@ualberta.ca>
2022-04-21 08:34:04 -0600

    a

--- core/train.py
+++ core/train.py
@@ -210,7 +210,7 @@ def train_net(cfg):
 
             encoder_loss2 = bce_loss(generated_projections, projections_images) * 10
             # encoder_loss = encoder_loss1 + encoder_loss2
-            if ((epoch_idx % 5) == 4):
+            if ((epoch_idx % 5) == 7):
                 encoder_loss = encoder_loss2
             else:
                 encoder_loss = encoder_loss1
