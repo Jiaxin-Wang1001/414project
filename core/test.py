06e55dc291e81d2c0785916e3af039436658a214
Jiaxin Wang <jiaxin13@ualberta.ca>
2022-04-22 09:09:27 -0600

    a

--- core/test.py
+++ core/test.py
@@ -161,6 +161,7 @@ def test_net(cfg,
                 # Volume Visualization
                 rendering_views = utils.helpers.get_volume_views(generated_volume.cpu().numpy())
                 # print("lalala", rendering_views.shape)
+                test_writer.add_images('Model%02d/Input' % sample_idx, rendering_images[0], epoch_idx, dataformats="HWC")
                 test_writer.add_images('Model%02d/Reconstructed' % sample_idx, rendering_views, epoch_idx, dataformats="HWC")
                 rendering_views = utils.helpers.get_volume_views(ground_truth_volume.cpu().numpy())
                 # print("lalala2", rendering_views.shape)
